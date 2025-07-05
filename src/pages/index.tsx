75f1624ded5a33705d9b584dcbd35794b1a32f8b
Aneesh-382005 <aneesh382005@gmail.com>
2025-07-05 13:59:08 +0530

    some dir changes

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../utils/supabaseClient'
+
+export default function HomePage() {
+  const [todos, setTodos] = useState<any[]>([])
+
+  useEffect(() => {
+    async function getTodos() {
+      const { data: todos, error } = await supabase.from('todos').select()
+      if (error) {
+        console.error(error)
+      } else {
+        setTodos(todos || [])
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+      <ul className="list-disc pl-6">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
